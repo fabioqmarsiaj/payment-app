b57ccfb3a4971a55652381e6e5fd57a4055f7230
Fabio Marsiaj <fabiobackend@gmail.com>
2023-11-09 17:59:15 -0300

    Separating db connectiong

---
+++ api/db/connect.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func Connect() (*mongo.Client, error) {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environment variable.")
+	}
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	return client, nil
+}
--- api/repositories/users.go
+++ api/repositories/users.go
@@ -1,31 +1,19 @@
 package repositories
 
 import (
+	"api/db"
 	"api/models"
 	"context"
 	"log"
-	"os"
 
-	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // insert an user into DB
 func Create(user models.User) (*mongo.InsertOneResult, error) {
+	
+	client, err := db.Connect()
 
-
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found")
-	}
-	uri := os.Getenv("MONGODB_URI")
-	if uri == "" {
-		log.Fatal("You must set your 'MONGODB_URI' environment variable.")
-	}
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
-	if err != nil {
-		panic(err)
-	}
 	defer func() {
 		if err := client.Disconnect(context.TODO()); err != nil {
 			panic(err)
